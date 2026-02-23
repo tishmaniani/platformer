7f38ebd0cf8d024d93170f5406380624f94e1f3d
Serho <supertishman@yandex.ru>
2026-02-23 19:53:38 +0300

    limited move camera

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
@@ -9,15 +10,28 @@ public class FollowCamera : MonoBehaviour
     private Vector3 offset = new Vector3(0, 0, -10);
     private Vector3 velocity = Vector3.zero;
 
+    //границы
+    private float minX = -5f;
+    private float maxX = 10000000f;
+
 
     void LateUpdate()
     {
-        if(target != null)
+        if (target != null)
         {
             float targetXOffset = flip.flipX ? -3f : 3f;
 
+
+            //Берем позицию игрока + оступ взависимости от того куда смотрит Player
             Vector3 cameraPos = new Vector3(target.position.x + targetXOffset, target.position.y, offset.z);
-            transform.position = Vector3.SmoothDamp(transform.position, cameraPos,ref velocity, smoothTime);
+
+            //Ставив ограничения на движения камеры по границам
+            float clampedX = Math.Clamp(cameraPos.x, minX, maxX);
+
+            // Финальная позиция
+            Vector3 finalPositionWithClamp = new Vector3(clampedX, cameraPos.y, offset.z);
+
+            transform.position = Vector3.SmoothDamp(transform.position, finalPositionWithClamp, ref velocity, smoothTime);
         }
     }
 
