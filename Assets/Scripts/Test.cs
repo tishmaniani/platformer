84c474efe49650d0bcb3fd59e1123962866f0f7b
Serho <supertishman@yandex.ru>
2026-02-23 13:36:04 +0300

    Fix: case sensitivity in Test.cs and ignore Library folder

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -5,13 +5,13 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.log("Проверка");
-        Debug.log("Проверка");
+        Debug.Log("Проверка");
+        Debug.Log("Проверка");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
